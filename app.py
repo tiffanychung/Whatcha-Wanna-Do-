c312c79c45b8e956ebfebb83d0419deb35fa14e5
tiffanychung <tiffchung@berkeley.edu>
2019-10-13 15:21:06 -0700

    first push of app.py

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import pprint
 token = secret()
 eventbrite = Eventbrite(token)
 pp = pprint.PrettyPrinter(indent=4)
+user = eventbrite.get_user()
+print(user['id'])
+print(user['name'])
 
 def eb_api_query():
     # dictionary keys of argument are the fields we care about
@@ -12,15 +15,36 @@ def eb_api_query():
 
     # do something like this...
     # argument["location.address"] =
-
-
+    argument['location.address'] = 'Berkeley'
+    argument['location.within'] = '5mi'
+    argument['start_date.range_start'] = "2019-09-29T00:00:00"
+    argument['categories'] = "103"
     #this line is to expand the venue field (necessary to retrieve address)
     argument["expand"] = "venue"
+    argument['price'] = "free"
 
-    eventbrite.event_search(**arguments)
+    #print("dictionary:", eventbrite.event_search(**argument)) #calling thes earch function on the arguments
+    #you have declared? either print or set to variable. this return a dictionary that returns all the events
     #populate this dictionary with info from the search
-    dict = {}
+    nonrelevantdict = eventbrite.event_search(**argument)
 
-    
+    events = []
+    for i in range(5):#len(nonrelevantdict)
+        dict  = {}
+        #retrieve name
+        dict['name'] = nonrelevantdict['events'][i]['name']['text']
+        #events.append(dict)
+        #retrieve url
+        dict['url'] = nonrelevantdict['events'][i]['url']
+        #retrieve description
+        dict['description'] = nonrelevantdict['events'][i]['description']['text']
+        #retrieve date
+        dict['date'] = nonrelevantdict['events'][i]['start']['local']
+        #retrieve image
+        dict['image'] = nonrelevantdict['events'][i]['logo']['original']['url']
+        #retrive location
+        dict['location'] = nonrelevantdict['location']['augmented_location']['city']
+        events.append(dict)
+    print(events)
 
 eb_api_query()
