2ae67982400ee44b0219cfa16c62afced7b5653d
tiffanychung <tiffchung@berkeley.edu>
2019-10-20 15:22:59 -0700

    pushing week2 hw

--- app.py
+++ app.py
@@ -9,19 +9,24 @@ user = eventbrite.get_user()
 print(user['id'])
 print(user['name'])
 
-def eb_api_query():
+def eb_api_query(*args):
     # dictionary keys of argument are the fields we care about
-    argument = {}
-
-    # do something like this...
-    # argument["location.address"] =
-    argument['location.address'] = 'Berkeley'
-    argument['location.within'] = '5mi'
-    argument['start_date.range_start'] = "2019-09-29T00:00:00"
-    argument['categories'] = "103"
-    #this line is to expand the venue field (necessary to retrieve address)
-    argument["expand"] = "venue"
-    argument['price'] = "free"
+    # argument = {}
+    #
+    argument['start_date.range_start'] = args[0]
+    argument['location.address'] = args[1]
+    argument['price'] = args[2]
+    argument['location.within'] = args[3]
+    argument['categories'] = args[4]
+    # # do something like this...
+    # # argument["location.address"] =
+    # argument['location.address'] = 'Berkeley'
+    # argument['location.within'] = '5mi'
+    # argument['start_date.range_start'] = "2019-09-29T00:00:00"
+    # argument['categories'] = "104"
+    # #this line is to expand the venue field (necessary to retrieve address)
+    # argument["expand"] = "venue"
+    # argument['price'] = "free"
 
     #print("dictionary:", eventbrite.event_search(**argument)) #calling thes earch function on the arguments
     #you have declared? either print or set to variable. this return a dictionary that returns all the events
@@ -29,11 +34,11 @@ def eb_api_query():
     nonrelevantdict = eventbrite.event_search(**argument)
 
     events = []
+    print(len(nonrelevantdict['events']))
     for i in range(5):#len(nonrelevantdict)
         dict  = {}
         #retrieve name
         dict['name'] = nonrelevantdict['events'][i]['name']['text']
-        #events.append(dict)
         #retrieve url
         dict['url'] = nonrelevantdict['events'][i]['url']
         #retrieve description
@@ -41,10 +46,9 @@ def eb_api_query():
         #retrieve date
         dict['date'] = nonrelevantdict['events'][i]['start']['local']
         #retrieve image
-        dict['image'] = nonrelevantdict['events'][i]['logo']['original']['url']
+        #dict['image'] =
+        print(nonrelevantdict['events'][i]['logo']['original']['url'])
         #retrive location
         dict['location'] = nonrelevantdict['location']['augmented_location']['city']
         events.append(dict)
-    print(events)
-
-eb_api_query()
+    return events
--- web.py
+++ web.py
@@ -1,12 +1,34 @@
 from flask import Flask, request, render_template
+from app import eb_api_query
 import json
 
 app = Flask(__name__)
 
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
 @app.route('/form-example', methods=['GET', 'POST']) #allow both GET and POST requests
 def form_example():
     if request.method == 'POST':
-        return;
+        # language = request.form.get('language')
+        # framework = request.form['framework']
+        radius = request.form.get('radius')
+        categories = request.form.get('categories')
+        date = request.form.get('date')
+        price = request.form.get('price')
+        location = request.form.get('location')
+        event_list =eb_api_query(date,location, price, radius, categories)
+        return render_template('index.html', name = event_list)
+
+
+        # return '''<h1>The language value is: {}</h1>
+        #           <h1>The framework value is: {}</h1>'''.format(language, framework)
+    # return '''<form method="POST">
+    #               Language: <input type="text" name="language"><br>
+    #               Framework: <input type="text" name="framework"><br>
+    #               <input type="submit" value="Submit"><br>
+    #           </form>'''
     return render_template('index.html')
 
 
